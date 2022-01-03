e8d723ea24570f3d3549d791e70d94e0cd7900a5
Mauro Saavedra <saa.mauro92@gmail.com>
2022-01-03 17:10:10 +0100

    chore: fixed responsive menu bar

--- components/Member/Member.js
+++ components/Member/Member.js
@@ -6,22 +6,23 @@ const Member = ({ data }) => {
 
 
     return (
+        <Link href={`/team/${data.attributes.slug}`}>
+            <div className="uk-card uk-card-default uk-card-large ">
+                <div className="uk-card-media-top">
+                    {data.attributes.image.data.attributes.url &&
 
-        <div className="uk-card uk-card-default uk-card-large ">
-            <div className="uk-card-media-top">
-                {data.attributes.image.data.attributes.url &&
 
-                    <Link href={`/team/${data.attributes.slug}`}>
                         <img src={data.attributes.image.data.attributes.url} alt="" width={600} height={400} uk-cover />
-                    </Link>
-                }
-            </div>
-            <div className="uk-card-body">
-                <h3 className="uk-card-title"> {data.attributes.name}</h3>
-                <p>{data.attributes.title}</p>
-            </div>
-        </div>
 
+                    }
+                </div>
+                <div className="uk-card-body">
+                    <h3 className="uk-card-title"> {data.attributes.name}</h3>
+                    <p>{data.attributes.title}</p>
+                </div>
+            </div>
+
+        </Link>
 
     )
 }
--- components/_App/Nav.js
+++ components/_App/Nav.js
@@ -2,6 +2,7 @@ import { React, useState, useEffect } from "react";
 import Link from "../../utils/ActiveLink";
 import logo from '../../assets/seven-seas-logo.png'
 import Image from 'next/image'
+
 function Nav() {
 
 
@@ -67,18 +68,34 @@ function Nav() {
                         <div className="uk-navbar-dropdown uk-navbar-dropdown-bottom-left" style={{ "left": "0px", "top": "80px" }}>
                             <ul className="uk-nav uk-navbar-dropdown-nav">
 
-                                <li className="uk-parent">
-                                    <a href="#">Parent</a>
+                                <Link href={"/courses/coastal-skipper"}>
+                                    <li className="uk-parent uk-margin">
+                                        <a >Coastal Skipper</a>
 
-                                </li>
-                                <li className="uk-parent">
-                                    <a href="#">Parent</a>
 
-                                </li>
-                                <li className="uk-parent">
-                                    <a href="#">Parent</a>
+                                    </li>
+                                </Link>
+                                <Link href={"/courses/day-skipper"}>
+                                    <li className="uk-parent uk-margin">
+                                        <a >Day Skipper</a>
 
-                                </li>
+
+                                    </li>
+                                </Link>
+                                <Link href={"/courses/day-skipper-motor"}>
+                                    <li className="uk-parent uk-margin">
+                                        <a >Day Skipper Motor</a>
+
+
+                                    </li>
+                                </Link>
+                                <Link href={"/courses/yachtmaster-exam-preparation"}>
+                                    <li className="uk-parent uk-margin">
+                                        <a >Yachtmaster Exam Preparation </a>
+
+
+                                    </li>
+                                </Link>
 
                             </ul>
                         </div>
@@ -91,15 +108,15 @@ function Nav() {
                         <div className="uk-navbar-dropdown uk-navbar-dropdown-bottom-left" style={{ "left": "0px", "top": "80px" }}>
                             <ul className="uk-nav uk-navbar-dropdown-nav">
 
-                                <li className="uk-parent">
+                                <li className="uk-parent uk-margin">
                                     <a href="#">Parent</a>
 
                                 </li>
-                                <li className="uk-parent">
+                                <li className="uk-parent uk-margin">
                                     <a href="#">Parent</a>
 
                                 </li>
-                                <li className="uk-parent">
+                                <li className="uk-parent uk-margin">
                                     <a href="#">Parent</a>
 
                                 </li>
@@ -115,10 +132,13 @@ function Nav() {
                         <div className="uk-navbar-dropdown uk-navbar-dropdown-bottom-left" style={{ "left": "0px", "top": "80px" }}>
                             <ul className="uk-nav uk-navbar-dropdown-nav">
 
-                                <li className="uk-parent">
-                                    <a href="#">Parent</a>
+                                <Link href={"/courses/coastal-skipper"}>
+                                    <li className="uk-parent">
+                                        <a >Coastal Skipper</a>
 
-                                </li>
+
+                                    </li>
+                                </Link>
                                 <li className="uk-parent">
                                     <a href="#">Parent</a>
 
@@ -148,8 +168,8 @@ function Nav() {
         /*         </div> */
     ) : (
 
-        <nav className="uk-navbar-container uk-margin uk-navbar" uk-navbar="">
 
+        <nav className="uk-navbar-container uk-margin uk-navbar" uk-navbar="">
             <div className="uk-navbar-left">
 
 
@@ -161,10 +181,13 @@ function Nav() {
                 </Link>
 
             </div>
+
+
             <div className="uk-navbar-right">
 
 
                 <ul className="uk-navbar-nav">
+
                     <div id="my-id" uk-offcanvas>
 
                         <a className="uk-navbar-toggle uk-icon uk-navbar-toggle-icon" uk-toggle="target: #offcanvas-nav-primary" href="#">
@@ -175,26 +198,48 @@ function Nav() {
                             </svg>
                         </a>
 
+
                         <div id="offcanvas-nav-primary" uk-offcanvas="overlay: true">
+                            <button className="uk-offcanvas-close uk-close-large uk-icon uk-close" type="button" uk-close="">
+                                <svg width="20" height="20" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+
+                                </svg>
+                            </button>
+
                             <div className="uk-offcanvas-bar uk-flex uk-flex-column">
                                 <Image src={logo} alt="logo" height={64} width={217} />
-                                <ul className="uk-nav uk-nav-primary uk-nav-center uk-margin-auto-vertical">
+                                <ul className="uk-nav uk-nav-primary uk-nav-left uk-margin-auto-vertical">
 
                                     <li className="uk-parent">
 
-                                        <a href="#">Parent</a>
-                                        <ul className="uk-nav-sub">
-                                            <li><a href="#">Sub item</a></li>
-                                            <li><a href="#">Sub item</a></li>
+
+                                        <ul className="uk-nav uk-nav-primary " uk-nav="">
+
+                                            <li className="uk-parent  " >
+                                                <a className="uk-margin" aria-expanded="false">Courses</a>
+                                                <ul className="uk-nav-sub" hidden="">
+                                                    <li><a className="uk-margin">Coastal Skipper </a></li>
+
+                                                    <li><a className="uk-margin">Sub </a></li>
+
+
+                                                </ul>
+                                            </li>
+
+                                            <li className="uk-parent  " >
+                                                <a className="uk-margin" aria-expanded="false">Activities {"&"} Services</a>
+                                                <ul className="uk-nav-sub" hidden="">
+                                                    <li><a className="uk-margin">Sub </a></li>
+                                                    <li><a className="uk-margin">Sub </a></li>
+
+
+                                                </ul>
+                                            </li>
+
+
                                         </ul>
                                     </li>
-                                    <li className="uk-parent">
-                                        <a href="#">Parent</a>
-                                        <ul className="uk-nav-sub">
-                                            <li><a href="#">Sub item</a></li>
-                                            <li><a href="#">Sub item</a></li>
-                                        </ul>
-                                    </li>
+
                                 </ul>
 
                             </div>
@@ -205,7 +250,9 @@ function Nav() {
 
 
             </div>
-        </nav>
+
+
+        </nav >
     )
 
 }
@@ -213,3 +260,32 @@ function Nav() {
 export default Nav
 
 
+
+
+/* 
+
+                   <ul className="uk-nav uk-nav-primary uk-nav-center uk-margin-auto-vertical uk-nav-parent-icon" uk-nav="">
+                            <li className="uk-active"><a href="#">Active</a></li>
+                            <li className="uk-parent">
+                                <a href="#" aria-expanded="false">Parent</a>
+                                <ul className="uk-nav-sub" hidden="">
+                                    <li><a href="#">Sub item</a></li>
+                                    <li><a href="#">Sub item</a></li>
+                                    <li><a href="#">Sub item</a></li>
+                                    <li><a href="#">Sub item</a></li>
+                                    <li><a href="#">Sub item</a></li>
+                                    <li><a href="#">Sub item</a>
+                                        <ul>
+                                            <li><a href="#">Sub item</a></li>
+                                            <li><a href="#">Sub item</a></li>
+                                        </ul>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li><a href="#js-options">Item</a></li>
+                            <li><a href="#">Item</a></li>
+                        </ul>
+
+
+
+*/
