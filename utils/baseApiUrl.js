dd5b23613af9734dc8bcad39ec65f4c761297dbd
Mauro Saavedra <saa.mauro92@gmail.com>
2022-01-05 19:31:21 +0100

    chore: fixed links

--- utils/baseApiUrl.js
+++ utils/baseApiUrl.js
@@ -1,6 +1,6 @@
 const baseApiUrl = process.env.NODE_ENV === "production"
-    ? 'http://localhost:1337'
-    : 'http://localhost:1337';
+    ? 'https://seven-seas-sailing.herokuapp.com'
+    : 'https://seven-seas-sailing.herokuapp.com';
 
 /*  https://seven-seas-sailing.herokuapp.com' */
 
