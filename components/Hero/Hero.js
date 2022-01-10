016ba07fdd33cd51741b882cf8c505f5e2dbb857
Mauro Saavedra <saa.mauro92@gmail.com>
2022-01-10 23:07:08 +0100

    chore:  added background with no Image component

--- components/Hero/Hero.js
+++ components/Hero/Hero.js
@@ -9,142 +9,154 @@ const Hero = ({ courses, images }) => {
     return (
         <>
 
+            <div
+                id="banner"
 
-            <div className="uk-height-expand uk-section uk-light uk-background-cover">
-
-                <Image src={images.background.data.attributes.url} priority alt="bg-image" layout="fill" objectFit="cover" />
-            </div>
-
-
-            <div className="uk-container uk-container-large  uk-flex uk-flex-wrap uk-flex-around uk-margin  " uk-scrollspy="cls: uk-animation-fade; target: .hero-cards; delay: 400; repeat: true">
-
+                className="uk-height-expand uk-section uk-light uk-background-cover "
+                data-src={images.background.data.attributes.url}
+                data-srcset={images.background.data.attributes.url}
+                data-sizes="(min-width: 650px) 650px, 100vw"
+                data-uk-img
+            >
 
 
-                <div className="uk-inline-clip hero-cards uk-margin">
-                    <Image src={images.sail.data.attributes.url} width={400} height={250} alt='' />
+                <div className="uk-container uk-container-large  uk-flex uk-flex-wrap uk-flex-around uk-margin  " uk-scrollspy="cls: uk-animation-fade; target: .hero-cards; delay: 400; repeat: true">
 
-                    <div className="uk-position-bottom uk-overlay uk-overlay-primary  " uk-toggle="target: .sail-toggle; mode: hover">
 
-                        <h2 >RYA Sail</h2>
 
-                        <ul className="sail-toggle" hidden>
+                    <div className="uk-inline-clip hero-cards uk-margin">
+                        <Image src={images.sail.data.attributes.url} width={400} height={250} alt='' />
 
-                            {courses.map((course, i) =>
-                                course.attributes.courseCategory === 'Sail' &&
+                        <div className="uk-position-bottom uk-overlay uk-overlay-primary  " uk-toggle="target: .sail-toggle; mode: hover">
 
-                                <Link href={`/courses/${course.attributes.slug}`} passHref key={i}>
-                                    <li>
-                                        <a href="">   {course.attributes.title}</a>
-                                    </li>
-                                </Link>
+                            <h2 >RYA Sail</h2>
 
-                            )}
-                        </ul>
+                            <ul className="sail-toggle" hidden>
 
+                                {courses.map((course, i) =>
+                                    course.attributes.courseCategory === 'Sail' &&
+
+                                    <Link href={`/courses/${course.attributes.slug}`} passHref key={i}>
+                                        <li>
+                                            <a href="">   {course.attributes.title}</a>
+                                        </li>
+                                    </Link>
+
+                                )}
+                            </ul>
+
+                        </div>
                     </div>
-                </div>
-                <div className="uk-inline-clip hero-cards uk-margin">
-                    <Image src={images.motor.data.attributes.url} width={400} height={250} alt='' />
+                    <div className="uk-inline-clip hero-cards uk-margin">
+                        <Image src={images.motor.data.attributes.url} width={400} height={250} alt='' />
 
-                    <div className="uk-position-bottom uk-overlay uk-overlay-primary " uk-toggle="target: .motor-toggle; mode: hover" >
-                        <h2 >RYA Motor</h2>
+                        <div className="uk-position-bottom uk-overlay uk-overlay-primary " uk-toggle="target: .motor-toggle; mode: hover" >
+                            <h2 >RYA Motor</h2>
 
-                        <ul className="motor-toggle" hidden >
+                            <ul className="motor-toggle" hidden >
 
-                            {courses.map((course, i) =>
-                                course.attributes.courseCategory === 'Motor' &&
+                                {courses.map((course, i) =>
+                                    course.attributes.courseCategory === 'Motor' &&
 
-                                <Link href={`/courses/${course.attributes.slug}`} passHref key={i}>
-                                    <li>
-                                        <a href="">   {course.attributes.title}</a>
-                                    </li>
-                                </Link>
+                                    <Link href={`/courses/${course.attributes.slug}`} passHref key={i}>
+                                        <li>
+                                            <a href="">   {course.attributes.title}</a>
+                                        </li>
+                                    </Link>
 
-                            )}
-                        </ul>
+                                )}
+                            </ul>
+                        </div>
                     </div>
-                </div>
-                <div className="uk-inline-clip hero-cards uk-margin">
-                    <Image src={images.power.data.attributes.url} width={400} height={250} alt='' />
+                    <div className="uk-inline-clip hero-cards uk-margin">
+                        <Image src={images.power.data.attributes.url} width={400} height={250} alt='' />
 
-                    <div className="uk-position-bottom uk-overlay uk-overlay-primary  " uk-toggle="target: .power-toggle; mode: hover" >
-                        <h2 >RYA Power</h2>
+                        <div className="uk-position-bottom uk-overlay uk-overlay-primary  " uk-toggle="target: .power-toggle; mode: hover" >
+                            <h2 >RYA Power</h2>
 
-                        <ul className="power-toggle" hidden >
+                            <ul className="power-toggle" hidden >
 
-                            {courses.map((course, i) =>
-                                course.attributes.courseCategory === 'Power' &&
+                                {courses.map((course, i) =>
+                                    course.attributes.courseCategory === 'Power' &&
 
-                                <Link href={`/courses/${course.attributes.slug}`} key={i} passHref >
-                                    <li>
-                                        <a href="">   {course.attributes.title}</a>
-                                    </li>
-                                </Link>
+                                    <Link href={`/courses/${course.attributes.slug}`} key={i} passHref >
+                                        <li>
+                                            <a href="">   {course.attributes.title}</a>
+                                        </li>
+                                    </Link>
 
-                            )}
-                        </ul>
-                    </div>
-                </div>
-            </div>
-
-
-
-            <div className=" uk-container uk-container-xsmall uk-flex uk-flex-column uk-flex-middle uk-position-relative  ">
-                <h2 className="uk-text-bolder action-text uk-text-center ">
-                    YOUR WAY AT SEA STARTS HERE!
-                </h2>
-
-                <button className="uk-button action-button uk-text-bolder uk-button-large uk-width-small uk-margin-bottom">JOIN US</button>
-            </div>
-
-
-
-            <div className="uk-container uk-container-large  uk-flex uk-flex-wrap uk-flex-around " uk-scrollspy="cls: uk-animation-fade; target: .hero-cards; delay: 400; repeat: true">
-
-                <div className="uk-inline-clip hero-cards uk-margin">
-                    <Image src={images.mile_building.data.attributes.url} width={400} height={250} alt='' />
-
-                    <div className="uk-position-bottom uk-overlay uk-overlay-primary  " uk-toggle="target: .mile-toggle; mode: hover"  >
-                        <h2 >RYA Mile Building</h2>
-
-                        <ul className="mile-toggle" hidden >
-
-                            {courses.map((course, i) =>
-                                course.attributes.courseCategory === 'Mile_Building' &&
-
-                                <Link href={`/courses/${course.attributes.slug}`} passHref key={i} >
-                                    <li>
-                                        <a href="">   {course.attributes.title}</a>
-                                    </li>
-                                </Link>
-
-                            )}
-                        </ul>
+                                )}
+                            </ul>
+                        </div>
                     </div>
                 </div>
 
 
-                <div className="uk-inline-clip hero-cards uk-margin">
-                    <Image src={images.jet_ski.data.attributes.url} width={400} height={250} alt='' />
 
-                    <div className="uk-position-bottom uk-overlay uk-overlay-primary  " uk-toggle="target: .jet-toggle; mode: hover" >
-                        <h2>RYA Jet Ski</h2>
+                <div className=" uk-container uk-container-xsmall uk-flex uk-flex-column uk-flex-middle   ">
+                    <h2 className="uk-text-bolder action-text uk-text-center ">
+                        YOUR WAY AT SEA STARTS HERE!
+                    </h2>
 
-                        <ul className="jet-toggle" hidden >
-
-                            {courses.map((course, i) =>
-                                course.attributes.courseCategory === 'Jet_Ski' &&
-
-                                <Link href={`/courses/${course.attributes.slug}`} passHref key={i}>
-                                    <li>
-                                        <a href="">   {course.attributes.title}</a>
-                                    </li>
-                                </Link>
-
-                            )}
-                        </ul>
-                    </div>
+                    <button className="uk-button action-button uk-text-bolder uk-button-large uk-width-small uk-margin-bottom">JOIN US</button>
                 </div>
+
+
+
+                <div className="uk-container uk-container-large  uk-flex uk-flex-wrap uk-flex-around " uk-scrollspy="cls: uk-animation-fade; target: .hero-cards; delay: 400; repeat: true">
+
+                    <div className="uk-inline-clip hero-cards uk-margin">
+                        <Image src={images.mile_building.data.attributes.url} width={400} height={250} alt='' />
+
+                        <div className="uk-position-bottom uk-overlay uk-overlay-primary  " uk-toggle="target: .mile-toggle; mode: hover"  >
+                            <h2 >RYA Mile Building</h2>
+
+                            <ul className="mile-toggle" hidden >
+
+                                {courses.map((course, i) =>
+                                    course.attributes.courseCategory === 'Mile_Building' &&
+
+                                    <Link href={`/courses/${course.attributes.slug}`} passHref key={i} >
+                                        <li>
+                                            <a href="">   {course.attributes.title}</a>
+                                        </li>
+                                    </Link>
+
+                                )}
+                            </ul>
+                        </div>
+                    </div>
+
+
+                    <div className="uk-inline-clip hero-cards uk-margin">
+                        <Image src={images.jet_ski.data.attributes.url} width={400} height={250} alt='' />
+
+                        <div className="uk-position-bottom uk-overlay uk-overlay-primary  " uk-toggle="target: .jet-toggle; mode: hover" >
+                            <h2>RYA Jet Ski</h2>
+
+                            <ul className="jet-toggle" hidden >
+
+                                {courses.map((course, i) =>
+                                    course.attributes.courseCategory === 'Jet_Ski' &&
+
+                                    <Link href={`/courses/${course.attributes.slug}`} passHref key={i}>
+                                        <li>
+                                            <a href="">   {course.attributes.title}</a>
+                                        </li>
+                                    </Link>
+
+                                )}
+                            </ul>
+                        </div>
+                    </div>
+
+                </div>
+
+
+
+
+
+
 
 
             </div>
