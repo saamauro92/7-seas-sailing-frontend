eccce1e231827dc303c6083db502239dea473e7f
Mauro Saavedra <saa.mauro92@gmail.com>
2022-01-19 14:18:34 +0100

    chore: testing sidebar responsive

--- components/NewsSidebar/NewsSidebar.js
+++ components/NewsSidebar/NewsSidebar.js
@@ -7,45 +7,45 @@ const NewsSidebar = ({ latestNews, courses }) => {
     return (
         <>
 
-
-            <div className="uk-card uk-card-secondary uk-card-body uk-position-top news_sidebar uk-flex uk-flex-baseline ">
-
-
-                {latestNews && latestNews.length > 0 &&
-
-                    < >
-                        <span className="uk-text-success">News! </span>
-                        <Link href={`/news/${latestNews[0].attributes.slug}`} passHref>
+            {
+                <div className="uk-card uk-card-secondary uk-card-body uk-position-top news_sidebar uk-flex uk-flex-baseline "  >
 
 
-                            <span> blog:  {" "}{latestNews[0].attributes.title}</span >
-                        </Link>
-                    </>
+                    {latestNews && latestNews.length > 0 &&
 
-                }
-
-                <span className="">Latest courses:  </span>
-                {courses && courses.length > 0 && courses.map((course, i) =>
-                    course.attributes.new_course &&
-                    <>
-
-                        <Link href={`/courses/${course.attributes.slug}`} passHref>
-
-                            <li> {course.attributes.title}</li>
-
-                        </Link>
-                    </>
-
-                )
+                        < >
+                            <span className="uk-text-success">News! </span>
+                            <Link href={`/news/${latestNews[0].attributes.slug}`} passHref>
 
 
+                                <span> blog:  {" "}{latestNews[0].attributes.title}</span >
+                            </Link>
+                        </>
+
+                    }
+
+                    <span className="">Latest courses:  </span>
+                    {courses && courses.length > 0 && courses.map((course, i) =>
+                        course.attributes.new_course &&
+                        <>
+
+                            <Link href={`/courses/${course.attributes.slug}`} passHref>
+
+                                <li> {course.attributes.title}</li>
+
+                            </Link>
+                        </>
+
+                    )
 
 
-                }
-
-            </div>
 
 
+                    }
+
+                </div>
+
+            }
 
 
 
