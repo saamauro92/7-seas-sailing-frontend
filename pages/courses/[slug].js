5eb4c741666332b496bdaefc122fa1eecb0c876b
Mauro Saavedra <saa.mauro92@gmail.com>
2022-02-03 14:52:39 +0100

    chore: added testimonials sector to courses

--- pages/courses/[slug].js
+++ pages/courses/[slug].js
@@ -1,3 +1,5 @@
+/* eslint-disable jsx-a11y/alt-text */
+/* eslint-disable @next/next/no-img-element */
 /* eslint-disable react/no-children-prop */
 
 import Link from 'next/link'
@@ -13,6 +15,9 @@ import ActionSection from '../../components/actionSection/ActionSection'
 
 const CourseSection = ({ course, homepage }) => {
     const imageUrl = course.attributes.image.data.attributes.url
+
+    const testimonials_banner = homepage.attributes.hero.testimonials_banner.data.attributes.formats.large.url;
+
 
 
     return (
@@ -146,8 +151,58 @@ const CourseSection = ({ course, homepage }) => {
 
 
 
+            </div>
 
 
+            <div className="uk-light uk-background-cover  testimonials_section uk-padding-large uk-padding-remove-horizontal" data-src={testimonials_banner}
+                data-srcset={testimonials_banner} data-uk-img>
+
+                <div className="uk-container">
+
+
+                    <h2 className='uk-text-center uk-text-italic  uk-text-bold'>HAPPY CLIENTS ABOUT US</h2>
+                    <div className="uk-position-relative uk-visible-toggle uk-dark" tabIndex="-1" uk-slider="sets: true">
+
+                        <ul className="uk-slider-items uk-child-width-1-1 uk-child-width-1-1@m">
+
+                            {course.attributes.testimonials.data.length > 0 && course.attributes.testimonials.data.map((item, i) =>
+                                <li key={i}>
+                                    <div className="uk-section uk-text-center" >
+                                        <article className="uk-comment">
+                                            <header className="uk-comment-header">
+                                                <div className="uk-grid-medium uk-flex-middle " uk-grid>
+                                                    <svg xmlns="http://www.w3.org/2000/svg" width="90" height="90" stroke="white" fill="white" viewBox="0 0 40 40"><path d="M19.092 6.863c-1.504 2.31-1.779 4.45-1.681 5.688 6.132-.101 5.696 6.449 1.39 6.449-1.83 0-3.801-1.338-3.801-4.275 0-2.724 1.412-5.845 4.092-7.862zm-13 0c-1.504 2.31-1.779 4.45-1.681 5.688 6.132-.101 5.696 6.449 1.39 6.449-1.83 0-3.801-1.338-3.801-4.275 0-2.724 1.412-5.845 4.092-7.862zm16.908-3.863c-6.108 1.206-10 6.584-10 11.725 0 3.97 2.786 6.275 5.801 6.275 2.615 0 5.199-1.797 5.199-4.979 0-2.601-1.905-4.757-4.396-5.149.217-2.004 2.165-4.911 4.38-5.746l-.984-2.126zm-13 0c-6.108 1.206-10 6.584-10 11.725 0 3.97 2.786 6.275 5.801 6.275 2.615 0 5.199-1.797 5.199-4.979 0-2.601-1.905-4.757-4.396-5.149.217-2.004 2.165-4.911 4.38-5.746l-.984-2.126z" /></svg>
+                                                    <div className="uk-comment-body uk-padding-large ">
+
+                                                        {item.attributes.description}
+                                                    </div>
+                                                </div>
+                                            </header>
+                                            <div className="uk-width-expand">
+                                                <h4 className="uk-comment-title uk-margin-remove "><a className="uk-link-reset" href="#"> {item.attributes.name}</a></h4>
+                                                <div className=" uk-margin-remove"><a className="uk-link-reset" href="#"> From {item.attributes.country}</a></div>
+
+                                            </div>
+                                        </article>
+
+                                    </div>
+                                </li>
+                            )
+                            }
+                        </ul>
+                        <a className="uk-position-bottom-left uk-position-small " href="#" uk-slidenav-previous uk-slider-item="previous">
+                            <svg width="60" height="80" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><polyline fill="none" stroke="#ccc" strokeWidth="1.33" points="13 16 7 10 13 4"></polyline></svg>
+
+
+                        </a>
+                        <a className="uk-position-bottom-right uk-position-small " href="#" uk-slidenav-next uk-slider-item="next">
+                            <svg width="60" height="80" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><polyline fill="none" stroke="#ccc" strokeWidth="1.33" points="7 4 13 10 7 16"></polyline></svg>
+
+
+                        </a>
+
+                    </div>
+                </div>
             </div>
 
 
