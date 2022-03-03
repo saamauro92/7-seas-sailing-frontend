fe704fda8b0049b819c540ee4cea403bbdc84c41
Mauro Saavedra <saa.mauro92@gmail.com>
2022-03-03 21:46:53 +0100

    chore: swap meta tag

--- components/_App/Layout.js
+++ components/_App/Layout.js
@@ -3,31 +3,25 @@ import Head from "next/head"
 import GoTop from './GoTop';
 import FooterStyleThree from './Footer';
 import Nav from './Nav';
-import NewsSidebar from '../NewsSidebar/NewsSidebar';
-import { useRouter } from 'next/router';
+import NextNProgress from "nextjs-progressbar";
 
 
 const Layout = ({ children, data, courses, activities, homepage, }) => {
-    const router = useRouter();
+
     // Preloader
-    /*     const [loader, setLoader] = React.useState(true);
-        React.useEffect(() => {
-            setTimeout(() => setLoader(false), 1500);
-        }, [])
-     */
+    const [loader, setLoader] = React.useState(true);
+    React.useEffect(() => {
+        setTimeout(() => setLoader(false), 1500);
+    }, [])
+
     return (
         < >
+
             <Head>
-                {/* Required meta tags */}
-                <meta charSet="utf-8" />
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
-                <link rel="icon" href="/favicon.ico" />
-                <title>Seven Seas Sailing</title>
             </Head>
-
-
-            {/*         {loader ? <h5> LOADING...</h5> : null} */}
-
+            {/*   <NextNProgress /> */}
+            {/*        {loader ? <NextNProgress /> : null} */}
             <Nav courses={courses} activities={activities} homepage={homepage} />
 
             {children}
--- pages/_document.js
+++ pages/_document.js
@@ -24,7 +24,7 @@ class MyDocument extends Document {
                         }}
                     />
                     <meta charSet="utf-8" />
-                    <meta name="viewport" content="width=device-width, initial-scale=1" />
+
                     <link rel="icon" href="/favicon.ico" />
                     <link
                         rel="stylesheet"
