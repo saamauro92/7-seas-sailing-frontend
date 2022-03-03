dc00b0259fa113434a20f3a2d831c22383bd7be6
Mauro Saavedra <saa.mauro92@gmail.com>
2022-03-03 12:50:36 +0100

    feat: google analitics added

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,24 @@
 import '../styles/globals.css'
 import Layout from '../components/_App/Layout'
 import { fetchAPI } from '../lib/api';
-
-
+import { useRouter } from 'next/router'
+import { useEffect } from 'react';
+import * as ga from '../lib/analytics'
 
 export default function MyApp({ Component, pageProps }) {
+
+  const router = useRouter()
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      ga.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
 
   const { courses, activities, homepage, testimonials } = pageProps;
 
--- pages/_document.js
+++ pages/_document.js
@@ -6,14 +6,28 @@ class MyDocument extends Document {
             <Html lang="zxx">
                 <Head>
                     {/* eslint-disable-next-line */}
+                    <script
+                        async
+                        src={`https://www.googletagmanager.com/gtag/js?id=G-7K5WWJY30P`}
+                    />
+                    <script
+                        dangerouslySetInnerHTML={{
+                            __html: `
+                                window.dataLayer = window.dataLayer || [];
+                                function gtag(){dataLayer.push(arguments);}
+                                gtag('js', new Date());
+                                gtag('config', 'G-7K5WWJY30P', {
+                                page_path: window.location.pathname,
+                                });
+                            `,
+                        }}
+                    />
                     <link
                         rel="stylesheet"
-                        href="https://fonts.googleapis.com/css?family=Staatliches"
+                        href="https://fonts.googleapis.com/css?family=Staatliches&display=optional"
                     />
 
                     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/uikit@3.11.1/dist/css/uikit.min.css" />
-
-
                     <script async src="https://cdn.jsdelivr.net/npm/uikit@3.11.1/dist/js/uikit.min.js"></script>
                     <script async src="https://cdn.jsdelivr.net/npm/uikit@3.11.1/dist/js/uikit-icons.min.js"></script>
 
